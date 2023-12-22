51edafe102ecfe2aa2020f04d110e1cfd1173635
evansochieng <evansochieng121@gmail.com>
2023-12-22 14:57:43 +0300

    practice running migrations using alembic

---
+++ lib/migrations/versions/e98f1f65eab4_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: e98f1f65eab4
+Revises: 
+Create Date: 2023-12-22 14:27:26.496194
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e98f1f65eab4'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
